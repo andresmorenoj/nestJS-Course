06c3cc03f168da35f95f173058fd2b369719c6e3
Andres Moreno <andresmorenojf@gmail.com>
2024-04-13 15:35:51 -0500

    chore: error handling for getting filtered tasks

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+
 import { TASKS_STATUS } from '../task.module';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TASKS_STATUS)
   status?: TASKS_STATUS;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
