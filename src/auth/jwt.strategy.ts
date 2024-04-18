a3a556b3a304b9d8417ba0a6b5062c8460312acd
Andres Moreno <andresmorenojf@gmail.com>
2024-04-18 15:47:15 -0500

    environment variable added for JWT secret

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -6,15 +6,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 import { User } from './user.entity';
 import { IJwtPayload } from './jwt-payload.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'topSecret51',
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
